b76418edb2d87ce197d971d8a2ee4191f49deacd
José Góngora <jgongora-2019063@kinal.edu.gt>
2021-03-04 16:52:26 -0600

    Se creó el archivo para las rutas de Users

---
+++ src/routes/user.routes.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require("express")
+const userController = require('../controllers/user.controller')
+const md_authentication = require('../middlewares/user.authenticated')
+
+var api = express.Router()
+
+api.post('/login', userController.login);
+api.post('/registerUser', md_authentication.ensureAuth, userController.registerUser);
+api.post('/registerClient', userController.registerUserClient);
+api.put('/editUser/:IdUser', md_authentication.ensureAuth, userController.editUser);
+api.delete('/deleteUser/:IdUser', md_authentication.ensureAuth, userController.deleteUser)
+
+module.exports = api
