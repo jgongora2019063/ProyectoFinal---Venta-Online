9714ca9b36dd07a0f8ad465e1b741d0931c3708e
José Góngora <jgongora-2019063@kinal.edu.gt>
2021-03-09 23:33:00 -0600

    Se agrego la ruta del Carrito

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const userController = require('./src/controllers/user.controller')
 const user_routes = require('./src/routes/user.routes')
 const category_routes = require('./src/routes/category.routes')
 const product_routes = require('./src/routes/product.routes')
+const cart_routes = require('./src/routes/shoppingCart.routes')
 
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
@@ -19,6 +20,7 @@ app.use(cors())
 app.use('/api', user_routes)
 app.use('/api', category_routes)
 app.use('/api', product_routes)
+app.use('/api', cart_routes)
 
 userController.createAdmin();
 
