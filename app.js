d11ce33d5fff353a961960ac4e700d654164c82d
José Góngora <jgongora-2019063@kinal.edu.gt>
2021-03-10 20:26:26 -0600

    Se hicieron algunos cambios

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const user_routes = require('./src/routes/user.routes')
 const category_routes = require('./src/routes/category.routes')
 const product_routes = require('./src/routes/product.routes')
 const cart_routes = require('./src/routes/shoppingCart.routes')
+const bill_routes = require('./src/routes/bill.routes')
 
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
@@ -21,6 +22,7 @@ app.use('/api', user_routes)
 app.use('/api', category_routes)
 app.use('/api', product_routes)
 app.use('/api', cart_routes)
+app.use('/api', bill_routes)
 
 userController.createAdmin();
 
